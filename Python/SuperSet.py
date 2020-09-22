0cfec30d3d267a35ec2dc84c60f7f325fda4f39f
Murtaza Shabbir <60929735+murtazashabbir53@users.noreply.github.com>
2020-09-22 10:54:05 +0530

    Update SuperSet.py

--- Python/SuperSet.py
+++ Python/SuperSet.py
@@ -1,6 +1,9 @@
 A = set(map(int,input().split()))
 
 n = int(input())
-N = set(map(int,input().split()))
-
-print(N.issuperset(A))
+status = True #assuring that A is subset of n sets
+for i in range(n):
+    N = set(map(int,input().split()))
+    current = N.issuperset(A) 
+    strict_suprset_result=status and current
+print(strict_suprset_result)    
