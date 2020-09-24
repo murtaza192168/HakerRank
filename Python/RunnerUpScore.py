e3a98d369f31bf30923f4f8e3ebba096701e771f
Murtaza Shabbir <60929735+murtazashabbir53@users.noreply.github.com>
2020-09-24 13:30:41 +0530

    Create RunnerUpScore.py

---
+++ Python/RunnerUpScore.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    first=max(arr)
+    
+    while(max(arr) == first):
+        arr.remove(max(arr))
+
+    print(max(arr)) 
+    
