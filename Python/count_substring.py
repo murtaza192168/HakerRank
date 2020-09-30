f43f29ebbfe8f916a4ea489e55d6bfa0b1af6d58
Murtaza Shabbir <60929735+murtazashabbir53@users.noreply.github.com>
2020-09-30 14:13:30 +0530

    Update count_substring.py

--- Python/count_substring.py
+++ Python/count_substring.py
@@ -1,9 +1,9 @@
 def count_substring(string, sub_string):
 
     count = 0
-    i = string.find(sub_string)
+    i = string.find(sub_string)          #find() is used to find the substring from the string
 
-    while i != -1:
+    while i != -1:                     #will return true, i.e. there should be atleast one substring. as indexing starts from 0, I hav put -1 as empty 
         count += 1
         i = string.find(sub_string , i+1)
             
@@ -16,5 +16,5 @@ if __name__ == '__main__':
     string = input().strip()
     sub_string = input().strip()
     
-    count = count_substring(string, sub_string)
+    count = count_substring(string, sub_string)       #calling function and display in the main
     print(count)
