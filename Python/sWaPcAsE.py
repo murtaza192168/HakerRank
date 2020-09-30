462affcb4d146d2acebeb5701502722f532228c0
Murtaza Shabbir <60929735+murtazashabbir53@users.noreply.github.com>
2020-09-30 11:15:03 +0530

    Create sWaPcAsE.py

---
+++ Python/sWaPcAsE.py
@@ -0,0 +1,11 @@
+def swap_case(s):
+    if s.islower():
+        return(s.swapcase())
+    elif(s.isupper):
+        return(s.swapcase())
+    return
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
