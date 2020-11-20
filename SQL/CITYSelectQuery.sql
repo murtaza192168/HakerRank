ac41ceb25a26175d8b53a8982aa8b004df1a66f3
Murtaza Shabbir <60929735+murtazashabbir53@users.noreply.github.com>
2020-11-20 10:42:46 +0530

    Update and rename SelectQuery.sql to CITYSelectQuery.sql

---
+++ SQL/CITYSelectQuery.sql
@@ -0,0 +1,33 @@
+--Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+--City TAble is as followed..
+--https://s3.amazonaws.com/hr-challenge-images/8137/1449729804-f21d187d0f-CITY.jpg
+
+SELECT *
+FROM CITY
+WHERE POPULATION > 100000 AND COUNTRYCODE  = 'USA';
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE='USA' AND POPULATION > 120000;
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--Query all columns for a city in CITY with the ID 1661.
+
+SELECT *
+FROM CITY
+WHERE ID = 1661;
+------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+
+SELECT *
+FROM CITY
+WHERE COUNTRYCODE = 'JPN';
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+--Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = 'JPN';
