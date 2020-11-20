710bfec7ce84680065dad1c62533b6dd36c02640
Murtaza Shabbir <60929735+murtazashabbir53@users.noreply.github.com>
2020-11-20 10:31:39 +0530

    Update SelectQuery.sql

--- SQL/SelectQuery.sql
+++ SQL/SelectQuery.sql
@@ -5,4 +5,13 @@
 SELECT *
 FROM CITY
 WHERE POPULATION > 100000 AND COUNTRYCODE  = 'USA';
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+--Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+--Table Image
+--https://s3.amazonaws.com/hr-challenge-images/8137/1449729804-f21d187d0f-CITY.jpg
+
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE='USA' AND POPULATION > 120000;
