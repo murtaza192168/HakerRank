886005f4a16c75f40abac4a159d610f6947f4427
Murtaza Shabbir <60929735+murtazashabbir53@users.noreply.github.com>
2020-11-20 10:31:08 +0530

    Delete SelectQuery2.sql

--- SQL/SelectQuery2.sql
+++
@@ -1,9 +0,0 @@
-
---Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
---Table Image
---https://s3.amazonaws.com/hr-challenge-images/8137/1449729804-f21d187d0f-CITY.jpg
-
-
-SELECT NAME
-FROM CITY
-WHERE COUNTRYCODE='USA' AND POPULATION > 120000;
