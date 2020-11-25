d7a85569af4569468d8f9050e69e0148336fa805
Murtaza Shabbir <60929735+murtazashabbir53@users.noreply.github.com>
2020-11-25 19:17:34 +0530

    Update and rename reservation1.sql to Weather Observation Stations.sql

--- SQL/Weather Observation Stations.sql
+++ SQL/Weather Observation Stations.sql
@@ -5,8 +5,14 @@ FROM STATION;
 ---------------------------------------------------------------------
 
 --Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
---The STATION table is described as follows:  https://s3.amazonaws.com/hr-challenge-images/9336/1449345840-5f0a551030-Station.jpg
+
 
 SELECT DISTINCT CITY
 FROM STATION
 WHERE MOD(ID, 2)=0;
+-----------------------------------------------------------------------------------------------------------------------------------------------
+
+--Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+SELECT   (COUNT(CITY) - COUNT(DISTINCT CITY) ) AS DIFFERENCE
+FROM STATION;
+------------------------------------------------------------------------------------------------------------------------------------
